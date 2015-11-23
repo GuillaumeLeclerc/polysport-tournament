39f95c959c7003c1fa6fbd90406a10d0dc609273
Guillaume Leclerc <guillaume.leclerc@epfl.ch>
2015-11-23 13:48:36 +0100

    display groups

--- src/components/tournamentDetails.vue
+++ src/components/tournamentDetails.vue
@@ -2,42 +2,100 @@
 <template>
 <div class="demo-updates mdl-card mdl-shadow--4dp mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--12-col-desktop">
   <div class="mdl-card__title mdl-card--expand mdl-color--teal-300">
-    <h2 class="mdl-card__title-text mdl-color-text--white">Welcome</h2>
-  </div>
-  <div class="mdl-card__supporting-text mdl-color-text--grey-600">
-    Displaying tournament {{id}}
+    <h2 class="mdl-card__title-text mdl-color-text--white">{{tournament.name}}</h2>
   </div>
   <div class="mdl-card__actions mdl-card--border">
-    <a href="#" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-color-text--teal-400">Read More</a>
+    <a href="#" @click.stop="this.mode='groups'" class="mdl-button mdl-button--raised mdl-js-button mdl-js-ripple-effect mdl-color-text--teal-400">Groups</a>
+    <a href="#" @click.stop="this.mode='tree'" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-color-text--teal-400">Final Tree</a>
   </div>
 </div>
 
-<div class="demo-updates notification mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-desktop" v-for="item in items">
-  <div class="cover mdl-card__title mdl-card--expand mdl-color--grey-100">
-    {{{item.sport | background}}}
+<div class="demo-updates notification mdl-card mdl-shadow--2dp mdl-cell mdl-cell--6-col mdl-cell--6-col-tablet mdl-cell--6-col-desktop" v-for="group in tournament.groupStats">
+  <div class="cover mdl-card__title mdl-card--expand mdl-color--primary">
+    <h2 class="mdl-card__title-text mdl-color-text--white">Group {{$index | letter}}</h2>
   </div>
-  <div class="mdl-card__supporting-text mdl-color-text--grey-600">
-    Your team <b>{{item.team}}</b> has a new match to play. <br>
-    Get ready !
-  </div>
-  <div class="mdl-card__actions mdl-card--border mdl-grid date-container">
-    <a href="#" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored mdl-js-ripple-effect"><i class="material-icons" role="presentation">date_range</i> Details</a> 
-    <div class="mdl-layout-spacer"></div>
-    <span class="date mdl-color-text--grey-700">June, 15th 2015</span>
+  <div class="mdl-color-text--grey-600">
+    <table class="mdl-data-table mdl-js-data-table group-stats">
+      <thead>
+        <tr>
+          <th class="mdl-data-table__cell--non-numeric">Team</th>
+          <th>V</th>
+          <th>D</th>
+          <th>T</th>
+          <th>Pts</th>
+        </tr>
+      </thead>
+      <tbody>
+      <tr v-for="team in group">
+        <td class="mdl-data-table__cell--non-numeric">{{team.name}}</td>
+        <td>{{team.wins}}</td>
+        <td>{{team.defeats}}</td>
+        <td>{{team.draws}}</td>
+        <td><b>{{team.points}}</b></td>
+      </tr>
+      </tbody>
+    </table>
   </div>
 </div>
 </template>
 
 <script>
 
-const sportsMap = require('../services/sports.js');
+const _ = require('lodash');
 
 export default {
-  data () {
-    return {
-    }
-  },
-  props: ['id']
+filters: {
+           letter(num) {
+             return String.fromCharCode(num + 65);
+           }
+         },
+           data () {
+             return {
+mode: 'groups',
+      tournament: [],
+             }
+           },
+props: ['id'],
+       ready() {
+         const allTournaments = require('../services/tournaments.js');
+         this.tournament = _.find(allTournaments, {id: parseInt(this.id)});
+         if (this.tournament.tree && this.tournament.tree.length > 0) {
+           this.mode= 'tree'
+         }
+
+
+         this.tournament.groupStats = _.map(this.tournament.groups, (g) => {
+             const teams = {}
+             _.each(g, (m) => {
+               _.each(m.teams, (t) => {
+                 if (!teams[t]) {
+                  teams[t] = {
+                    name: t,
+                    wins: 0,
+                    defeats: 0,
+                    draws: 0,
+                    points: 0
+                  };
+                 }
+               });
+               if (m.scores[0] === m.scores[1]) {
+                teams[m.teams[0]].draws++;
+                teams[m.teams[1]].draws++;
+              } else {
+                const winnerId = m.scores[0] > m.scores[1]? 0: 1;
+                const looserId = 1 - winnerId;
+                teams[m.teams[winnerId]].wins++;
+                teams[m.teams[looserId]].defeats++;
+              }
+               teams[m.teams[0]].points += m.points[0];
+               teams[m.teams[1]].points += m.points[1];
+            });
+           return _.sortBy(teams, '-points');
+         });
+
+         console.log(this.infos);
+       }
+
 }
 </script>
 
@@ -53,6 +111,10 @@ export default {
     .date {
       @include align-self(center);
     }
+  }
+
+  .group-stats {
+    width:100%;
   }
 }
 </style>
---
+++ src/services/tournaments.js
@@ -0,0 +1,33 @@
+/* vim: set softtabstop=2 shiftwidth=2 expandtab : */
+module.exports = [{
+  id: 1,
+  name: 'This is a super football tournament',
+  sport: 'football',
+  groups: [
+    [{
+      teams: ['Les branlos', 'Les rigolos'],
+      scores: [4, 4],
+      points: [1, 1]
+    }, {
+      teams: ['Les branlos', 'Les nuls'],
+      scores: [4, 0],
+      points: [2, 0]
+    }, {
+      teams: ['Les rigolos', 'Les nuls'],
+      scores: [5, 0],
+      points: [2, 0]
+    }], [{
+      teams: ['Les branlos', 'Les rigolos'],
+      scores: [4, 4],
+      points: [1, 1]
+    }, {
+      teams: ['Les branlos', 'Les nuls'],
+      scores: [4, 0],
+      points: [2, 0]
+    }, {
+      teams: ['Les rigolos', 'Les nuls'],
+      scores: [5, 0],
+      points: [2, 0]
+    }]
+  ]
+}]
