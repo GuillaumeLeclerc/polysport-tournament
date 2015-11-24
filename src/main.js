d928786c1133358c8d93b9771f3c49bce322ad21
Guillaume Leclerc <guillaume.leclerc@epfl.ch>
2015-11-24 09:45:22 +0100

    chrome fixed

--- src/main.js
+++ src/main.js
@@ -48,6 +48,10 @@ router.map({
     }
 })
 
+router.redirect({
+  '*': '/notifications'
+});
+
 // Now we can start the app!
 // The router will create an instance of App and mount to
 // the element matching the selector #app.
